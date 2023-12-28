1dbcbf048ccfff8e3495c8cce9a3963a378b63b6
Iuri Pereira <iuripsiqueira@gmail.com>
2023-12-28 16:05:49 -0300

    Movimento da torre ajustado

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -27,6 +27,7 @@ public class ChessMatch {
         Position source = sourcePosition.toPosition();
         Position target = targetPosition.toPosition();
         validateSourcePosition(source);
+        validateTargetPosition(source, target);
         Piece capturedPiece = makeMove(source, target);
         return (ChessPiece) capturedPiece;
     }
@@ -41,6 +42,15 @@ public class ChessMatch {
     private void validateSourcePosition(Position position){
         if (!board.thereIsAPiece(position)){
             throw new ChessException("Nao existe peça na posição de origem");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("Nao existe movimentos possiveis para a peça escolhida");
+        }
+    }
+
+    private void validateTargetPosition(Position source, Position target){
+        if(!board.piece(source).possibleMove(target)){
+            throw new ChessException("A posição de origem nn pode de mover para posição de destino");
         }
     }
 
